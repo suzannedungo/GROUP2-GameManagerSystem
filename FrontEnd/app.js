905d84b4b8c189b7386fa34a363e75b119806fa5
Mark0612-ui <2022312044@dhvsu.edu.ph>
2024-11-23 08:22:19 +0800

    FrontEnd added animations

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,18 +1,18 @@
 const arrows = document.querySelectorAll(".arrow");
-const movieLists = document.querySelectorAll(".movie-list");
+const gameLists = document.querySelectorAll(".game-list");
 
 arrows.forEach((arrow, i) => {
-  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  const itemNumber = gameLists[i].querySelectorAll("img").length;
   let clickCounter = 0;
   arrow.addEventListener("click", () => {
     const ratio = Math.floor(window.innerWidth / 270);
     clickCounter++;
     if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
-      movieLists[i].style.transform = `translateX(${
-        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      gameLists[i].style.transform = `translateX(${
+        gameLists[i].computedStyleMap().get("transform")[0].x.value - 300
       }px)`;
     } else {
-      movieLists[i].style.transform = "translateX(0)";
+      gameLists[i].style.transform = "translateX(0)";
       clickCounter = 0;
     }
   });
@@ -20,11 +20,12 @@ arrows.forEach((arrow, i) => {
   console.log(Math.floor(window.innerWidth / 270));
 });
 
+
 //TOGGLE
 
 const ball = document.querySelector(".toggle-ball");
 const items = document.querySelectorAll(
-  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+  ".container,.game-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
 );
 
 ball.addEventListener("click", () => {
@@ -33,5 +34,3 @@ ball.addEventListener("click", () => {
   });
   ball.classList.toggle("active");
 });
-
-
