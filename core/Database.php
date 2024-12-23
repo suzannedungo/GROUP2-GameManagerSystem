5a11b634ecb938cb3adcc4b868936965d5576374
Kobe Tuazon <2022311035@dhvsu.edu.ph>
2024-12-23 08:29:53 +0800

    Fixing for hosting

--- core/Database.php
+++ core/Database.php
@@ -9,6 +9,8 @@ class Database {
   public static function connect() {
     $pdo = null;
     $server_name = null;
+    $user = null;
+    $pass = null;
     $db_name = null;
     if(
       $_SERVER["SERVER_NAME"] === "localhost" || 
@@ -16,13 +18,17 @@ class Database {
       $_SERVER["SERVER_ADDR"] === "192.168.1.72") {
       $server_name = "localhost";
       $db_name = "game_manager";
+      $user = "root";
+      $pass = "";
     } else {
       $server_name = "localhost";
-      $db_name = "";
+      $db_name = "u175342239_game_manager";
+      $user = "u175342239_user";
+      $pass = "MySQL_root_12345";
     }
 
     try {
-      $pdo = new PDO("mysql:host={$server_name};dbname={$db_name}", "root", "");
+      $pdo = new PDO("mysql:host={$server_name};dbname={$db_name}", $user, $pass);
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(Exception $pdo_err) {
       echo 
